04b614f9d1a8f30e0739d4548a1d063bb8d3a457
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-06-02 07:37:50 +0300

    IDistibutedCache interface added

---
+++ IDistributedCacheRedisApp.Web/Controllers/ProductsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using System;
+using System.Threading.Tasks;
+
+namespace IDistributedCacheRedisApp.Web.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IDistributedCache _distributedCache;
+
+        public ProductsController(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+        }
+
+   
+    }
+}
