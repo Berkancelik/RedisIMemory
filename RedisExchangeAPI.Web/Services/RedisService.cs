fe1ab11a1601ad3a536edbf2d9101ff7e88dd662
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-06-04 18:02:07 +0300

    StackExchange.Redis API

---
+++ RedisExchangeAPI.Web/Services/RedisService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Configuration;
+using StackExchange.Redis;
+
+namespace RedisExchangeAPI.Web.Services
+{
+    public class RedisService
+    {
+
+        private readonly string _redisHost;
+        private readonly string _redisPort;
+        //redis server üzeriden aşağıdaki class sayesinde haberleşebiliriz.
+        private ConnectionMultiplexer _redis;
+        public IDatabase db { get; set; }
+
+        public RedisService(IConfiguration configuration)
+        {
+            
+            _redisHost = configuration["Redis:Host"];
+            _redisPort = configuration["Redis:Port"]; ;
+        }
+
+        public void Connect()
+        {
+            var configString = $"{_redisHost}: {_redisPort}";
+            _redis = ConnectionMultiplexer.Connect(configString);
+        }
+
+        public IDatabase GetDb(int db)
+        {
+            return _redis.GetDatabase(db);
+        }
+    }
+}
--- RedisExchangeAPI.Web/Startup.cs
+++ RedisExchangeAPI.Web/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using RedisExchangeAPI.Web.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,11 +24,12 @@ namespace RedisExchangeAPI.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<RedisService>();
             services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, RedisService redisService)
         {
             if (env.IsDevelopment())
             {
@@ -45,7 +47,7 @@ namespace RedisExchangeAPI.Web
             app.UseRouting();
 
             app.UseAuthorization();
-
+            redisService.Connect();
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
