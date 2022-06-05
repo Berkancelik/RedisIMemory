3c5a2f79ac3df7c84d6958140d2c3eb481d966ed
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-06-05 15:58:38 +0300

    Redis List

---
+++ RedisExchangeAPI.Web/Controllers/ListTypeController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RedisExchangeAPI.Web.Services;
+using StackExchange.Redis;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedisExchangeAPI.Web.Controllers
+{
+ 
+    public class ListTypeController : Controller
+    {
+        private readonly RedisService _redisService;
+        private readonly IDatabase db;
+        private string listKey = "names";
+        public ListTypeController(RedisService redisService)
+        {
+            _redisService = redisService;
+            db = _redisService.GetDb(0);
+        }
+        public IActionResult Index()
+        {
+            List<string> nameList = new List<string>();
+            if (db.KeyExists(listKey))
+            {
+                db.ListRange(listKey).ToList().ForEach(x =>{
+                    nameList.Add(x.ToString());
+                });
+            }
+            return View(nameList);
+        }
+
+
+        [HttpPost]
+        public IActionResult Add(string name)
+        {
+            db.ListRightPush(listKey,name);
+            return RedirectToAction("Index");
+        }
+
+
+        public IActionResult DeleteItem(string name)
+        {
+            db.ListRemoveAsync(listKey, name).Wait();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteFirstItem()
+        {
+            db.ListLeftPop(listKey);
+            return RedirectToAction("Index");
+        }
+    }
+}
