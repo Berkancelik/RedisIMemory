185db0dc27b175cf5bca15840b6a2a73080eb8dd
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-06-06 20:00:26 +0300

    Redis Set

---
+++ RedisExchangeAPI.Web/Controllers/SetTypeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RedisExchangeAPI.Web.Services;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedisExchangeAPI.Web.Controllers
+{
+    public class SetTypeController : Controller
+    {
+        private readonly RedisService _redisService;
+        private readonly IDatabase db;
+        private string listKey = "hashnames";
+        public SetTypeController(RedisService redisService)
+        {
+            _redisService = redisService;
+            db = _redisService.GetDb(3);
+        }
+        public IActionResult Index()
+        {
+            HashSet<string> nameList = new HashSet<string>();
+            if (db.KeyExists(listKey))
+            {
+                db.SetMembers(listKey).ToList().ForEach(x =>
+                {
+                    nameList.Add(x.ToString());
+                });
+            }
+            return View(nameList);
+        }
+
+        [HttpPost]
+        public IActionResult Add(string name)
+        {
+
+            db.KeyExpire(listKey, DateTime.Now.AddMinutes(5));
+            db.SetAdd(listKey, name);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> DeleteItem(string name)
+        {
+            await db.SetRemoveAsync(listKey, name);
+            return RedirectToAction("Index");
+        }
+    }
+}
