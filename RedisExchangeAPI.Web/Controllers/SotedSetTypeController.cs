9068056fc9c7b3f429d268eb186485a143eeb020
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-06-11 16:36:55 +0300

    Redis Sort Set

---
+++ RedisExchangeAPI.Web/Controllers/SotedSetTypeController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RedisExchangeAPI.Web.Services;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RedisExchangeAPI.Web.Controllers
+{
+    public class SotedSetTypeController : Controller
+    {
+        private readonly RedisService _redisService;
+        private readonly IDatabase db;
+        private string listKey = "sorterdsetnames";
+        public SotedSetTypeController(RedisService redisService)
+        {
+            _redisService = redisService;
+            db = _redisService.GetDb(3);
+        }
+        public IActionResult Index()
+        {
+            HashSet<string> list = new HashSet<string>();
+            if (db.KeyExists(listKey))
+            {
+                db.SortedSetScan(listKey).ToList().ForEach(x =>
+                {
+                    list.Add(x.ToString());
+                });
+
+                db.SortedSetRangeByRank(listKey, 0, 5, order: Order.Descending).ToList().ForEach(x =>
+                {
+                    list.Add(x.ToString());
+                });
+            }
+            return View(list);
+        }
+        [HttpPost]
+        public IActionResult Add(string name, int score)
+        {
+            db.KeyExpire(listKey, DateTime.Now.AddMinutes(1));
+            db.SortedSetAdd(listKey, name, score);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteItem(string name)
+        {
+            db.SortedSetRemove(listKey, name);  
+            return RedirectToAction("Index");
+        }
+    }
+}
